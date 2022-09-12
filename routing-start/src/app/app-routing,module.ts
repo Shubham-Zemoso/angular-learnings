8aaa6e9b38003a286481df011668bc83b8a34489
shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-09-12 16:21:01 +0530

    hash locations

--- routing-start/src/app/app-routing,module.ts
+++ routing-start/src/app/app-routing,module.ts
@@ -39,9 +39,8 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+//   imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
