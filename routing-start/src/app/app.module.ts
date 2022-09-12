54a449de0328b42e9b63879a827cea2a56e27657
shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-09-12 14:43:32 +0530

    added routes

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -10,13 +10,10 @@ import { UserComponent } from "./users/user/user.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersService } from "./servers/servers.service";
-import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing,module";
 
-const appRoutes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "users", component: UsersComponent },
-  { path: "servers", component: ServersComponent },
-];
+
 
 @NgModule({
   declarations: [
@@ -27,8 +24,9 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
