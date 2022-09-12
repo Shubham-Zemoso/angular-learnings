bcbe1817ffb0bb3fdf44f077db3b3701b0f2bcd9
shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-09-12 16:00:33 +0530

    adding static data

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { AppRoutingModule } from "./app-routing,module";
 import { AuthGuard } from "./auth-guard.service";
 import { AuthService } from "./auth.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-ga=uard.service";
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 
 
@@ -28,6 +29,7 @@ import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-ga=uard
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
+    ErrorPageComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
