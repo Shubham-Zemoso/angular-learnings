071bc780ffbb6a8bc3e9a566d8a751dabb97b855
shubham-zemoso <shubham.kukreti@zemosolabs.com>
2022-09-18 17:40:22 +0530

    added env variables

--- my-project/src/app/auth/auth.service.ts
+++ my-project/src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from 'rxjs/operators';
 import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 export interface AuthResponseData {
   kind: string;
@@ -27,7 +28,7 @@ export class AuthService {
   signup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDQ2GvzYKeVW67KyKoJLgQnj7u_fk9jONg',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseApiKey,
         {
           email: email,
           password: password,
@@ -51,7 +52,7 @@ export class AuthService {
   login(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDQ2GvzYKeVW67KyKoJLgQnj7u_fk9jONg',
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseApiKey,
         {
           email: email,
           password: password,
--- my-project/src/environments/environment.prod.ts
+++ my-project/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseApiKey: 'AIzaSyDQ2GvzYKeVW67KyKoJLgQnj7u_fk9jONg',
 };
